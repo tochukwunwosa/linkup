77d857e2ba5cf4f0a827cf9c4bf48ca284634046
Tochukwu Nwosa <tochukwunwosa28@gmail.com>
2026-02-03 16:02:48 +0100

    feat: Add more pages to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -34,5 +34,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 0.6,
     },
+    {
+      url: `${siteUrl}/privacy-policy`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.3,
+    },
+    {
+      url: `${siteUrl}/terms-of-use`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.3,
+    },
   ];
 }
