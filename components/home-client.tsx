ac8b8b3e2c5d05440cc21f8974e8c8c9e943fb99
Tochukwu Nwosa <tochukwunwosa28@gmail.com>
2026-02-22 06:37:50 +0100

    refac: remove dynamic import for UpcomingEvents component to simplify code

--- components/home-client.tsx
+++ components/home-client.tsx
@@ -1,14 +1,9 @@
 "use client";
 
-import dynamic from "next/dynamic";
 import Hero from "@/components/hero";
 import Filters from "@/components/filters";
+import UpcomingEvents from "@/components/upcoming-events";
 import { Event } from "@/lib/validations/event";
-
-// Lazy load below-the-fold components
-const UpcomingEvents = dynamic(() => import("@/components/upcoming-events"), {
-  ssr: false, // Disable SSR for this since we're passing server data
-});
 
 interface HomeClientProps {
   initialEvents: Event[];
